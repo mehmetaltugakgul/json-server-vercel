2cee8ef3c8a5a2c20003bc9dfff7c0fecb183af3
Mehmet Altuğ Akgül <mehmetaltug.akgul@ogr.iu.edu.tr>
2023-03-08 14:22:21 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,9 +7,10 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+  '/tasks': '/api/tasks',
+  '/tasks/:id': '/api/tasks/:id',
+  '/tasks/:id/show': '/api/tasks/:id'
+}));
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
